390eda48ad76f3ed948d7d09cd3b228db6994b0a
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-06 13:32:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma (phrase)
   elsif phrase == "I LOVE YOU GRANDMA"
       return "I LOVE YOU TOO PUMPKIN!"
     else
-      return "NO, NO SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     end
 end
 
