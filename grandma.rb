cdd73318a7751be6c54c65af3ea7a66cf77c065e
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-06 13:31:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma (phrase)
-  if !phrase == phrase.upcase
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA"
       return "I LOVE YOU TOO PUMPKIN!"
